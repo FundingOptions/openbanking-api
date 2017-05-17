1a13580b1acccb01f1c8f3cab69a47809127e3f4
Jon Staley <jmstaley@gmail.com>
2017-05-17 11:25:47 +0000

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='openbanking',
+    version='0.1.0',
+    description='Simple wrapper for openbanking',
+    long_description='',
+    author='Jon Staley',
+    author_email='jon.staley@fundingoptions.com',
+    url='',
+    license='',
+    py_modules=['openbanking'],
+    install_requires=['requests']
+)
