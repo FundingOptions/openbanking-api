29cc229d606874dd01acb5c64e42a9040a7960e7
Jon Staley <jmstaley@gmail.com>
2017-05-18 15:16:07 +0000

    changes methods to only handle one bank at a time and changes participant for bank

--- openbankingapi.py
+++ openbankingapi.py
@@ -1,37 +1,34 @@
 import requests
 
-PARTICIPANT_LIST_URL = 'https://developer.openbanking.org.uk/open-data/participant-store/participant_store.json'
+BANK_LIST_URL = 'https://developer.openbanking.org.uk/open-data/participant-store/participant_store.json'
 
 
 class OpenBankingApi:
     def __init__(self, timeout=5):
         self.timeout = timeout
 
-    def get_api_data(self, endpoint, participants):
-        data = {}
-        for participant in participants:
-            if participant.get('supportedAPIs', {}).get(endpoint):
-                url = '{0}/{1}/{2}'.format(
-                    participant['baseUrl'],
-                    participant['supportedAPIs'][endpoint][0],
-                    endpoint
-                )
+    def get_api_data(self, endpoint, bank):
+        if bank.get('supportedAPIs', {}).get(endpoint):
+            url = '{0}/{1}/{2}'.format(
+                bank['baseUrl'],
+                bank['supportedAPIs'][endpoint][0],
+                endpoint
+            )
 
-                response = requests.get(url, timeout=self.timeout)
+            response = requests.get(url, timeout=self.timeout)
 
-                data[participant['name']] = response.json()
-        return data
+            return response.json()
 
-    def participants(self):
-        response = requests.get(PARTICIPANT_LIST_URL, timeout=self.timeout)
+    def banks(self):
+        response = requests.get(BANK_LIST_URL, timeout=self.timeout)
 
         return response.json().get('data', [])
 
-    def unsecured_sme_loans(self, participants):
-        return self.get_api_data('unsecured-sme-loans', participants)
+    def unsecured_sme_loans(self, bank):
+        return self.get_api_data('unsecured-sme-loans', bank)
 
-    def business_current_accounts(self, participants):
-        return self.get_api_data('business-current-accounts', participants)
+    def business_current_accounts(self, bank):
+        return self.get_api_data('business-current-accounts', bank)
 
-    def commercial_credit_cards(self, participants):
-        return self.get_api_data('commercial-credit-cards', participants)
+    def commercial_credit_cards(self, bank):
+        return self.get_api_data('commercial-credit-cards', bank)
