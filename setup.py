da31cf809bb5386895162ac619e22f48203d1dab
Jon Staley <jmstaley@gmail.com>
2017-05-18 10:02:42 +0000

    add license and update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
 
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='openbanking',
     version='0.1.0',
     description='Simple wrapper for openbanking',
-    long_description='',
+    long_description=readme,
     author='Jon Staley',
     author_email='jon.staley@fundingoptions.com',
     url='',
-    license='',
+    license=license,
     py_modules=['openbanking'],
     install_requires=['requests']
 )
