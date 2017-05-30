a89d78a8518408b42c4143a85de98dd8ee67ec3c
Jon Staley <jmstaley@gmail.com>
2017-05-30 10:57:24 +0000

    improves method name

--- openbankingapi.py
+++ openbankingapi.py
@@ -7,7 +7,7 @@ class OpenBankingApi:
     def __init__(self, timeout=5):
         self.timeout = timeout
 
-    def get_api_data(self, endpoint, bank):
+    def get_bank_api_data(self, endpoint, bank):
         if bank.get('supportedAPIs', {}).get(endpoint):
             url = '{0}/{1}/{2}'.format(
                 bank['baseUrl'],
@@ -25,10 +25,10 @@ class OpenBankingApi:
         return response.json().get('data', [])
 
     def unsecured_sme_loans(self, bank):
-        return self.get_api_data('unsecured-sme-loans', bank)
+        return self.get_bank_api_data('unsecured-sme-loans', bank)
 
     def business_current_accounts(self, bank):
-        return self.get_api_data('business-current-accounts', bank)
+        return self.get_bank_api_data('business-current-accounts', bank)
 
     def commercial_credit_cards(self, bank):
-        return self.get_api_data('commercial-credit-cards', bank)
+        return self.get_bank_api_data('commercial-credit-cards', bank)
