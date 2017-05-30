636c954d635e627280217bc7e85a7e4b82f8a26b
Jon Staley <jmstaley@gmail.com>
2017-05-30 16:12:39 +0000

    adds missing manifest file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='openbankingapi',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple wrapper for openbanking',
     long_description=readme,
     author='Funding Options',
