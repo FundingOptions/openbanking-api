ab2d0f55c588ef30bb01dc6ca69a5df97bd66776
Jon Staley <jmstaley@gmail.com>
2018-04-05 12:58:56 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='openbankingapi',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple wrapper for openbanking',
     long_description=readme,
     author='Funding Options',
