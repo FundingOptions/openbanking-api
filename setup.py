c04c74243db310a53eeb6b6dbd5ba27e96dc8427
Jon Staley <jmstaley@gmail.com>
2019-05-15 12:23:42 +0000

    bump version number and add long description content type

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ with open('README.md') as f:
 
 setup(
     name='openbankingapi',
-    version='0.1.3',
+    version='0.1.5',
     description='Simple wrapper for openbanking',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Funding Options',
     author_email='techsupport@fundingoptions.com',
     url='https://github.com/FundingOptions/openbanking-api',
